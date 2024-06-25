4f40daa722edb475a596b9d461c5b420d26783bd
Dan Gilleland <dagilleland@shaw.ca>
2024-06-25 11:53:40 -0600

    Tweak the Base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Programming-0101.github.com/TheCuratedGuideToJavaScript',
-	base: '/TheCuratedGuideToJavaScript/',
+	base: 'TheCuratedGuideToJavaScript',
 	integrations: [
 		starlight({
 			title: 'Your JS Guide',
